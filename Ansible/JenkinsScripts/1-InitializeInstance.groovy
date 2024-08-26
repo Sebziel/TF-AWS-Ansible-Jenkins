2db9647367fbb0153ef2b511a0b8f5e3519577de
Sebziel <sebzie1996@gmail.com>
2024-08-26 15:17:05 +0000

    adding init scripts, modyfing playbook not to start jenkins

--- Ansible/JenkinsScripts/1-InitializeInstance.groovy
+++ Ansible/JenkinsScripts/1-InitializeInstance.groovy
@@ -1,6 +1,10 @@
 import jenkins.model.*
 import jenkins.install.*
 
+println("Running init script to set install state")
+
 def instance = Jenkins.getInstance()
 instance.setInstallState(InstallState.INITIAL_SETUP_COMPLETED)
 instance.save()
+
+println("Install state set to INITIAL_SETUP_COMPLETED")
