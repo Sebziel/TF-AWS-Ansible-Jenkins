20909f75405686387b0ea9c95940dc3aaf4ceb72
Sebziel <sebzie1996@gmail.com>
2024-08-27 08:25:22 +0000

    adjusting create admin logic to wait for matrix plugin install

--- Ansible/JenkinsScripts/2-CreateAdminUser.groovy
+++ Ansible/JenkinsScripts/2-CreateAdminUser.groovy
@@ -2,16 +2,39 @@ import jenkins.model.*
 import hudson.security.*
 
 def instance = Jenkins.getInstance()
+def pm = instance.getPluginManager()
+def uc = instance.getUpdateCenter()
 
-// Set up the Security Realm
-def hudsonRealm = new HudsonPrivateSecurityRealm(false)
-hudsonRealm.createAccount("admin", "admin_password")
-instance.setSecurityRealm(hudsonRealm)
+def isPluginInstalled(pm) {
+    return pm.getPlugin("matrix-auth")
+}
 
-// Set up the Authorization Strategy (Matrix-based)
-def strategy = new GlobalMatrixAuthorizationStrategy()
-strategy.add(Jenkins.ADMINISTER, "admin")
-instance.setAuthorizationStrategy(strategy)
+int maxRetries = 10
+int retries = 0
 
-println("Created Admin user with admin permissions")
-instance.save()
+while (retries < maxRetries) {
+    if (isPluginInstalled(pm)) {
+        println "Plugin is installed. Proceeding to the next part of the script."
+        def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+        hudsonRealm.createAccount("admin", "admin_password")
+        instance.setSecurityRealm(hudsonRealm)
+        
+        def strategy = new GlobalMatrixAuthorizationStrategy()
+        strategy.add(Jenkins.ADMINISTER, "admin")
+        instance.setAuthorizationStrategy(strategy)
+        
+        println("Created Admin user with admin permissions")
+        instance.save()
+
+        break
+    } else {
+        println "Plugin is not installed. Sleeping for 1 second..."
+        sleep(5000)
+        retries++
+    }
+}
+
+if (retries == maxRetries) {
+    println "Plugin is still not installed after 10 retries. Exiting script."
+    // Add any cleanup or exit logic here if needed
+}
